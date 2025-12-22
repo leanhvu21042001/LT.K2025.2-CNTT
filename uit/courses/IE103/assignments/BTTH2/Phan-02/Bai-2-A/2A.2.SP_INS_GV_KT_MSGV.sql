02bbb2238bef90fb735797988bd2b23a1ffed7bc
Sam Dinh <25410291@uit.edu.vn>
2025-12-22 21:47:33 +0800

    Update 2A.2.SP_INS_GV_KT_MSGV.sql

--- uit/courses/IE103/assignments/BTTH2/Phan-02/Bai-2-A/2A.2.SP_INS_GV_KT_MSGV.sql
+++ uit/courses/IE103/assignments/BTTH2/Phan-02/Bai-2-A/2A.2.SP_INS_GV_KT_MSGV.sql
@@ -3,11 +3,11 @@
 --Tham số vào là MSGV, TENGV, SODT, DIACHI, MSHH, NAMHH.
 --Trước khi insert dữ liệu cần kiểm tra MSGV trong table GIAOVIEN có trùng không, nếu trùng thì trả về giá trị 0.
 CREATE PROC SP_INS_GV_KT_MSGV
-    @MSGV INT, 
-	@TENGV NVARCHAR(30), 
+    @MSGV INT,
+	@TENGV NVARCHAR(30),
 	@SODT VARCHAR(10),
-    @DIACHI NVARCHAR(50), 
-	@MSHH INT, 
+    @DIACHI NVARCHAR(50),
+	@MSHH INT,
 	@NAMHH SMALLDATETIME
 AS
 BEGIN
