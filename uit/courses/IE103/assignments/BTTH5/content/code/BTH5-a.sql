b6f73e99acf4ac625acf316f8d9cec61145291db
sam-uit <25410291@ms.uit.edu.vn>
2026-01-14 16:44:52 +0800

    Update BTH5-a.sql

--- uit/courses/IE103/assignments/BTTH5/content/code/BTH5-a.sql
+++ uit/courses/IE103/assignments/BTTH5/content/code/BTH5-a.sql
@@ -15,7 +15,6 @@ GO
 INSERT INTO KhoaHoc (TenKhoaHoc) SELECT 'Mang May Tinh Truyen Thong'
 INSERT INTO KhoaHoc (TenKhoaHoc) SELECT 'Khoa Hoc May Tinh'
 INSERT INTO KhoaHoc (TenKhoaHoc) SELECT 'Ky Thuat May Tinh'
-
 GO
 
 -- SinhVien
